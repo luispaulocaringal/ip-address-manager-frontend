46f56beb090a61994a59d51533aaa93bdaa000ff
Luis Caringal <luis.caringal@senarysoft.com>
2025-08-17 14:02:14 +0800

    added home details

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,22 @@
 function Home() {
   return (
-    <>Home</>
+    <div>
+      <h1>Home</h1>
+      <p>This is a summary of things you need to know about this web application.</p>
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Welcome to the IP Management System</h5>
+          <hr/>
+          <p className="card-text">This application allows you to manage IP addresses, view audit logs, and perform various administrative tasks.</p>
+          <p className="card-text">Use the navigation menu to access different sections of the application.</p>
+          <hr/>
+          <p className="card-text">
+            Demo project created by <strong>Luis Caringal</strong> for his application for <strong>Senior Full Stack Developer</strong> role at <strong>Techlint</strong>. This project uses <strong>ReactJS</strong> as its frontend framework,
+            and <strong>PHP Laravel</strong> for the backend server. The database used is <strong>MySQL</strong>. This project is contained in a <strong>Docker</strong> container, which can be run locally or deployed to a server.
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
